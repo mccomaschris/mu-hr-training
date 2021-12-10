c8e4abfc713113aef01b79134d73b0448da122d1
Christopher McComas <mccomas.chris@gmail.com>
2021-12-10 08:14:29 -0500

    add email to registration confirmation

--- mu-hr-training.php
+++ mu-hr-training.php
@@ -285,7 +285,22 @@ function mu_hr_registration_submitted_registration( $post_id ) {
 		),
 	);
 
-	// send email
+	if ( get_field( 'muhr_registration_email_address', $post_id ) ) {
+		$training_session = get_post( get_field( 'muhr_registration_training_session', $post_id ) );
+
+		$course_name       = get_field( '', $training_session->ID );
+		$course_location   = get_field( 'mu_training_training_location', $training_session->ID );
+		$course_day        = Carbon::parse( get_field( 'mu_training_start_time', $training_session->ID ) )->format( 'F j, Y' );
+		$course_start_time = Carbon::parse( get_field( 'mu_training_start_time', $training_session->ID ) )->format( 'g:i a' );
+		$course_end_time   = Carbon::parse( get_field( 'mu_training_end_time', $training_session->ID ) )->format( 'g:i a' );
+
+		$email_body  = 'You have successfully registered for ' . $course_name . ' at ' . $course_location . ' on ' . $course_day . ' at ' . $course_start_time . ' - ' . $course_end_time;
+		$email_body .= ".\r\r";
+		$email_body .= 'For any questions please contact Human Resources.';
+
+		$headers = 'From: human-resources@marshall.edu';
+		mail( get_field( 'muhr_registration_email_address', $post_id ), 'HR Training Registration', $email_body, $headers );
+	}
 }
 add_action( 'acf/save_post', 'mu_hr_registration_submitted_registration', 15 );
 
